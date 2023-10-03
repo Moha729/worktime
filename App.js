394f6ddac59584ecc51a06b9a288b4c4567df98c
moha <moha729g@stud.kea.dk>
2023-10-03 20:01:19 +0200

    Ready to implement functionality

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import { app, database } from './firebase';
+import { useState } from 'react';
 
 export default function App() {
   //add new work
 
+  const [addNewDialog, setAddNewDialog] = useState(null)
+
   return (
     <View style={styles.container}>
-      <AppBar style={styles.container}/>
-      <AppBody style={styles.container}/>
+      <AppBar />
+      <AppBody 
+        
+        addNewDialog={addNewDialog}
+        setAddNewDialog={setAddNewDialog} />
       <StatusBar style="auto" />
     </View>
   );
@@ -23,27 +29,54 @@ const AppBar = () => {
   )
 }
 
-const AppBody = () => {
+const AppBody = (props) => {
+
+  const addNewDialog = props.addNewDialog
+  const setAddNewDialog = props.setAddNewDialog
 
   return (
-    <View>
-      <DataBar />
-      <AddNewForm />
+    <View style={styles.container}>
+      <DataBar 
+        addNewDialog={addNewDialog}
+        setAddNewDialog={setAddNewDialog} />
+      {
+        addNewDialog &&
+        <AddNewForm />
+      }
       <DataItems />
     </View>
   )
 }
 
-const DataBar = () => {
+const DataBar = (props) => {
+
+  const addNewDialog = props.addNewDialog
+  const setAddNewDialog = props.setAddNewDialog
+
+  //TO-DO button does not have click effect
+  function openAddNewDialog(){
+    setAddNewDialog(true)
+  }
 
   return (
-    <View style={styles.tableRow}>
-      <Button 
-        style={styles.tableCell}
-        title='Add new' />
-      <Text style={styles.tableCell}>Start</Text>
-      <Text style={styles.tableCell} >End</Text>
-      <Text style={styles.tableCell} >Total</Text>
+    <View style={[styles.tableRow, {paddingBottom: 10}]}>
+      <Pressable
+        onPress={openAddNewDialog}
+        style={styles.defaultButton} > 
+          <Text style={styles.buttonText}>Add</Text>
+      </Pressable>
+      <Text style={[styles.tableCell, {
+        fontSize: 20,
+        fontWeight: '600'
+      }]}>Start</Text>
+      <Text style={[styles.tableCell, {
+        fontSize: 20,
+        fontWeight: '600'
+      }]} >End</Text>
+      <Text style={[styles.tableCell, {
+        fontSize: 20,
+        fontWeight: '600'
+      }]} >Total</Text>
     </View>
   )
 }
@@ -52,7 +85,7 @@ const AddNewForm = () => {
 
   return (
     <View style={styles.tableRow}>
-      <TextInput style={[styles.textinput, styles.tableCell]} />
+      <TextInput style={[styles.textinput]} />
       <Button
         style={styles.tableCell} 
         title='Go' />
@@ -72,9 +105,22 @@ const DataItems = () => {
         data={data}
         renderItem={(item) => 
           <View style={styles.tableRow}>
-            <Text style={styles.tableCell}>{item.item.name}</Text>
-            <Text style={styles.tableCell}>Edit</Text>
-            <Text style={styles.tableCell}>Edit</Text>
+            <Text style={[styles.tableCell, {
+              fontSize: 20,
+              fontWeight: '600'
+            }]}>{item.item.name}</Text>
+            <TextInput 
+              defaultValue='Edit'
+              style={[styles.tableCell, {
+                borderColor: "blue",
+                borderWidth: 1
+              }]} />
+            <TextInput 
+              defaultValue='Edit'
+              style={[styles.tableCell, {
+                borderColor: "blue",
+                borderWidth: 1
+              }]} />
             <Text style={styles.tableCell}>Total</Text>
           </View>}/>
     </View>
@@ -89,8 +135,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   headerBar: {
-    paddingTop: 25,
-    paddingBottom: 35,
+    paddingTop: 150,
+    paddingBottom: 0,
 
   },
   headerText: {
@@ -104,10 +150,28 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   tableCell: {
-    padding: 20
+    paddingTop: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 1
   },
   textinput: {
     backgroundColor: 'lightblue',
-    width: '70%'
+    width: '70%',
+    borderColor: "blue",
+    borderWidth: 1
+  },
+  defaultButton: {
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
   }
 });
